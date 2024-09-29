6c134716f618e817cf5dbefad801d29c34735968
7lucyseven <7lucyseven@gmail.com>
2024-09-29 15:14:03 +0900

    わんこめのAPIからコメントを取得できるように変更

--- src/get_comment.py
+++ src/get_comment.py
@@ -1,27 +1,34 @@
 import json
 import logging
+import requests
 
 def init():
     while(1):
         try:
-            json_open = open('C:/Users/Creator/AppData/Roaming/live-comment-viewer/comment.json', 'r', encoding="utf-8")
-            json_load = json.load(json_open)
+            #json_open = open('C:/Users/Creator/AppData/Roaming/live-comment-viewer/comment.json', 'r', encoding="utf-8")
+            #json_load = json.load(json_open)
+            json_load = requests.get('http://localhost:11180/api/comments')
             break
         except Exception as e:
             # print(e)
             continue
 
-    return json_load
+    return json_load.json()
 
 def data(json_load):
+    #comments = [entry['data']['comment'] for entry in json_load]
+    #print('##TEST')
+    #print(comments)
+    #print('##TEST')
     l_data = []
-    for i in json_load['comments']:
+    #for i in json_load['comments']:
+    for i in json_load:
         l_tmp = []
         l_tmp.append(i['data']['timestamp'])
         l_tmp.append(i['data']['name'])
         l_tmp.append(i['data']['comment'])
         l_data.append(l_tmp)
-
+        
     return l_data 
 
 def is_new(l_data, num):
--- src/sister_vocevox.py
+++ src/sister_vocevox.py
@@ -49,6 +49,7 @@ def speak(sister_respons):
         # 指定のデバイスインデックスを検索
         for i in range(5,14):
             if('Yamaha SYNCROOM Driver' in p.get_device_info_by_index(i)['name']):
+            #if('Open' in p.get_device_info_by_index(i)['name']):
                 output_device_index = i
                 break
 
