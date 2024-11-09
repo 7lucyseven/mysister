852f91efe2451803665d027a8aa462da68b71f87
7lucyseven <7lucyseven@gmail.com>
2024-11-09 13:38:19 +0900

    2024/11/09 排他制御、スレッドセーフ、変数の改善

--- flask_app/mysister.py
+++ flask_app/mysister.py
@@ -2,6 +2,8 @@ import time
 import os
 import sys
 import copy
+import threading
+import fcntl
 
 import importlib
 sys.dont_write_bytecode = True
@@ -21,21 +23,54 @@ from watchdog.events import PatternMatchingEventHandler
 import configparser
 
 class mysister:
-    logger = setup_logger(__name__)
-    config = configparser.ConfigParser()
-    config.read("dynamic_property.ini")
-
     def __init__(self):
+        # ロガーの初期化
+        self.logger = setup_logger(__name__)
+        
+        # 設定ファイルの初期化
+        self.config = configparser.ConfigParser()
+        self._read_config_with_lock()
+        
+        # スレッドセーフのためのロック
+        self._lock = threading.Lock()
+        self._config_lock = threading.Lock()
+        
+        # コメント番号の初期化
         self.num = 0
-        self.observer = None
-        self.observer_02 = None
+        
+        # 監視オブジェクトの初期化
+        self.observer = None  # lucy_text.txt監視用
+        self.observer_02 = None  # 設定ファイル監視用
+        
+        # モード番号の初期化
+        with self._lock:
+            self.mode_num = int(self.config["BASE"]["mode_num"])
+
+    def _read_config_with_lock(self):
+        """設定ファイルを排他的にロックして読み込む"""
+        with open("dynamic_property.ini", "r") as f:
+            fcntl.flock(f.fileno(), fcntl.LOCK_EX)
+            try:
+                self.config.read_file(f)
+            finally:
+                fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+
+    def _write_config_with_lock(self):
+        """設定ファイルを排他的にロックして書き込む"""
+        with open("dynamic_property.ini", "w") as f:
+            fcntl.flock(f.fileno(), fcntl.LOCK_EX)
+            try:
+                self.config.write(f)
+            finally:
+                fcntl.flock(f.fileno(), fcntl.LOCK_UN)
 
     def on_modified(self, event):
         """lucy_text.txtの監視処理"""
         filepath = event.src_path
         filename = os.path.basename(filepath)
         self.logger.info('%s changed' % filename)
-        lucy_communication()
+        with self._lock:
+            lucy_communication()
 
     def on_modified_02(self, event):
         """設定ファイルの監視処理"""
@@ -43,15 +78,19 @@ class mysister:
         filename = os.path.basename(filepath)
         self.logger.info('%s changed' % filename)
 
-        self.config.read("dynamic_property.ini")
-        status = self.config["BASE"]["status"]
-        self.logger.info('status is ' + status)
+        with self._lock:
+            with self._config_lock:
+                self._read_config_with_lock()
+                status = self.config["BASE"]["status"]
+                self.mode_num = int(self.config["BASE"]["mode_num"])
+                self.logger.info('status is ' + status)
+                self.logger.info('mode_num is ' + str(self.mode_num))
 
-        if(status == "stop"):
-            self.logger.info('stop')
-            sys.exit()
+                if(status == "stop"):
+                    self.logger.info('stop')
+                    sys.exit()
 
-        self.logger.info('設定ファイルのタイムスタンプに変更があったため、新しく設定ファイルを読み込みました。')
+                self.logger.info('設定ファイルのタイムスタンプに変更があったため、新しく設定ファイルを読み込みました。')
 
     def run(self):
         """メイン実行処理"""
@@ -66,33 +105,34 @@ class mysister:
         self._setup_config_observer()
         self.logger.info('設定ファイルの監視開始')
 
-        mode_num = int(self.config["BASE"]["mode_num"])
         self.logger.info('設定ファイルの初回読み込み完了')
 
         # コメントデータの初期取得
-        json = get_comment.init()
-        l_data = get_comment.data(json)
-        self.logger.info('すでにあるコメントをすべて取得しました。')
+        with self._lock:
+            json = get_comment.init()
+            l_data = get_comment.data(json)
+            self.logger.info('すでにあるコメントをすべて取得しました。')
         
         self.logger.info('メインのループ処理に入ります。')
 
         try:
             while True:
-                if mode_num == 1:
-                    continue
-                elif mode_num == 2:
-                    self._handle_mode2(l_data)
-                    json = get_comment.init()
-                    l_data = get_comment.data(json)
-                elif mode_num == 3:
-                    self._handle_mode3(l_data) 
-                    json = get_comment.init()
-                    l_data = get_comment.data(json)
-                elif mode_num == 4:
-                    self._handle_mode4()
-                else:
-                    self.logger.info('exit')
-                    exit()
+                with self._lock:
+                    if self.mode_num == 1:
+                        continue
+                    elif self.mode_num == 2:
+                        self._handle_mode2(l_data)
+                        json = get_comment.init()
+                        l_data = get_comment.data(json)
+                    elif self.mode_num == 3:
+                        self._handle_mode3(l_data) 
+                        json = get_comment.init()
+                        l_data = get_comment.data(json)
+                    elif self.mode_num == 4:
+                        self._handle_mode4()
+                    else:
+                        self.logger.info('exit')
+                        exit()
                 time.sleep(1)
 
         except KeyboardInterrupt:
@@ -128,7 +168,7 @@ class mysister:
 
     def _handle_mode3(self, l_data):
         """モード3の処理"""
-        print("while(3):")
+        self.logger.info("while(3):")
         lucy_communication()
         if get_comment.is_new(l_data, self.num):
             self.logger.info('モード[3]：新規コメントがありました。')
@@ -138,6 +178,6 @@ class mysister:
         """モード4の処理"""
         os.system('cls')
         sister_respons = sister_ai.respons("楽しい話題の雑談を行ってください")
-        print("フォルトゥナちゃんからの雑談")
-        print(sister_respons)
+        self.logger.info("フォルトゥナちゃんからの雑談")
+        self.logger.info(sister_respons)
         sister_vocevox.speak('' + ';' + sister_respons)
